9d1046f182666d174bd38202e1960f44c8edd00f
Hasini E <hasini1530@gmail.com>
2025-05-23 10:55:58 +0530

    Add files via upload

--- Java_Script.js
+++ Java_Script.js
@@ -1,9 +1,25 @@
+
 function a(){
-    var a=document.getElementById("a").value;
-    var res="";
-    for(var i=0;i<=a.length;i++)
-{
-    res=a.charAt(i)+res;
+    var v=document.getElementById("a").value;
+    document.write(v.toUpperCase(),"<br>");
 }
-document.write(res);
+function b(){
+    var v=document.getElementById("a").value;
+    document.write(v.toLowerCase(),"<br>");
+}
+function c(){
+    var v=document.getElementById("a").value;
+    document.write(v.includes(hey),"<br>");
+}
+function d(){
+    var v=document.getElementById("a").value;
+    document.write(v.indexOf("h"),"<br>");
+}
+function e(){
+    var v=document.getElementById("a").value;
+    document.write(v.replace("h","v"),"<br>");
+}
+function f(){
+    var v=document.getElementById("a").value;
+    document.write(v.split(" "),"<br>");
 }
