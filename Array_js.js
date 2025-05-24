41a564f2dcaa8a8daa0ac5bc16d51fba070a4da5
Hasini E <hasini1530@gmail.com>
2025-05-24 11:20:14 +0530

    Add files via upload

---
+++ Array_js.js
@@ -0,0 +1,10 @@
+function a()
+{
+    var s=["Apple","Mango","Guava"];
+    for(var i=0;i<s.length;i++)
+    {
+        document.writeln(s[i]);
+    }
+    document.writeln("<br>")
+    document.writeln(s);
+}
