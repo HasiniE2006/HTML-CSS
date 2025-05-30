4700c4371a0b24a8b1b9bcf1f9f44e3dc71496a6
Hasini E <hasini1530@gmail.com>
2025-05-30 14:30:16 +0530

    Without_div.js

---
+++ Without_div.js
@@ -0,0 +1,18 @@
+//App.js
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+ return(
+  <>
+  <h1>Hey</h1>
+  <h2>Paragraph</h2>
+  <h3>I</h3>
+  <h4>Am</h4>
+  <h5>Busy</h5>
+  <h6>Here</h6>
+  </>
+ )
+}
+
+export default App;
