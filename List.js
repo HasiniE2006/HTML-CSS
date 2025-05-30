690deaadb343fe7faffa7292179f85399e2584cf
Hasini E <hasini1530@gmail.com>
2025-05-30 14:27:33 +0530

    List.js

---
+++ List.js
@@ -0,0 +1,21 @@
+//App.js
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+ return(
+  <>
+  <h1>Hey</h1>
+  <a href='https://www.youtube.com'>click me</a>
+  <ol start='a' type='a'>
+      <li>Apple</li>
+      <li>Banana</li>
+  </ol>
+  <ul type='square'>
+      <li>Apple</li>
+      <li>Banana</li>
+  </ul>
+  </>
+ )
+}
+export default App;
