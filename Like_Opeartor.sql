1ee70454a08578f2be1331ff0e653ab3a218d5dd
Hasini E <hasini1530@gmail.com>
2025-06-05 15:02:15 +0530

    Like_Opeartor.sql

--- Like_Opeartor.sql
+++ Like_Opeartor.sql
@@ -4,9 +4,13 @@ create table st(
     sa int
 );
 insert into st values
-    (1,"Raju",30),
+    (1,"",30),
     (2,"Sam",24),
     (3,"Raj",33),
     (4,"Navs",19),
     (5,"Rekha",22);
-select * from st where sn like "s%";
+select * from st where sn like "s%";/*start with s*/
+select * from st where sn like "%a";/*end with a*/
+select * from st where sn like "";/*null*/
+select * from st where sn is not NULL;
+select * from st where sn like '_a_';
