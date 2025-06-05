1ef3744dbdd440f146878c138f26d5f1c027dc8b
Hasini E <hasini1530@gmail.com>
2025-06-05 12:34:58 +0530

    Student_Details.sql

---
+++ Student_Details.sql
@@ -0,0 +1,17 @@
+create table students(
+    sid number,
+    sname char,
+    sdob date,
+    sjoin year,
+    sphno number,
+    sadd varchar
+);
+insert into students(sid,sname,sdob,sjoin,sphno,sadd)
+values(3001,"ram","2000-06-05","2025",1234567890,"10-75 plmr"),
+    (3002,"sita","2000-06-05","2025",1234512345,"10-75 mpl"),
+    (3003,"ravan","2001-06-05","2025",1234555555,"10-75 plmr");
+select * from students;
+//output
+3001|ram|2000-06-05|2025|1234567890|10-75 plmr
+3002|sita|2000-06-05|2025|1234512345|10-75 mpl
+3003|ravan|2001-06-05|2025|1234555555|10-75 plmr
